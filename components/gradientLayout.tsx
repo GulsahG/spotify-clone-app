6830d0be985f47d9f689b400b2fcdedddb8b0088
GulsahG <gulsahgenc.gg@gmail.com>
2022-04-17 23:43:33 +0300

    Added artists layout

--- components/gradientLayout.tsx
+++ components/gradientLayout.tsx
@@ -14,9 +14,9 @@ const GradientLayout = ({
     <Box
       height="100%"
       overflowY="auto"
-      bgGradient={`linear(${color}.500 0%, ${color}.600 10%, ${color}.700 20%, ${color}.800 30%, rgba(0,0,0,0.95) 65%)`}
+      bgGradient={`linear(${color}.500 0%, ${color}.600 10%, ${color}.700 20%, ${color}.800 25%, ${color}.900 40%, rgba(0,0,0,0.95) 65%)`}
     >
-      <Flex p="40px" align="end">
+      <Flex bg={`${color}.800`} p="40px 40px 25px 40px" align="end">
         <Box padding="20px">
           <Image
             src={avatar}
@@ -27,15 +27,23 @@ const GradientLayout = ({
           />
         </Box>
         <Box padding="20px" lineHeight="40px" color="white">
-          <Text fontSize="x-small" fontWeight="bold" casing="uppercase">
+          <Text
+            mb="15px"
+            fontSize="x-small"
+            fontWeight="bold"
+            casing="uppercase"
+          >
             {subtitle}
           </Text>
-          <Text fontSize="7xl" fontWeight="bold">
+          <Text mb="20px" fontSize="7xl" fontWeight="bold">
             {title}
           </Text>
-          <Text fontSize="x-small">{description}</Text>
+          <Text mt="10px" fontSize="x-small">
+            {description}
+          </Text>
         </Box>
       </Flex>
+      <Box paddingY="50px">{children}</Box>
     </Box>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import Head from "next/head";
-import Image from "next/image";
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
+
 import styles from "../styles/Home.module.css";
 import GradientLayout from "../components/gradientLayout";
+import prisma from "../lib/prisma";
 
-export default function Home() {
+const Home = ({ artists }) => {
   return (
     <GradientLayout
       color="brown"
@@ -13,7 +16,48 @@ export default function Home() {
       description="19 Public Playlists - 106 Followers"
       avatar="https://i.scdn.co/image/ab6775700000ee8508d489fbe80dd6c89e0c2b70"
     >
-      <div>homepage</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="30px">
+          <Text fontSize="xl" fontWeight="bold">
+            Top artists this month
+          </Text>
+          <Text fontSize="x-small" mt="2px" color="gray.400">
+            Only visible to you
+          </Text>
+        </Box>
+        <Flex>
+          {artists.map((artist, idx) => (
+            <Box
+              key={artist.id}
+              width="calc(14.29% - 40px)"
+              marginLeft={idx === 0 ? "0" : "40px"}
+            >
+              <Box width="100%" bg="gray.900" borderRadius="4px" padding="15px">
+                <Image
+                  src="https://i.scdn.co/image/ab6761610000e5ebd6f2323c1971fd5a70cd0255"
+                  borderRadius="100%"
+                />
+                <Box m="20px 0 10px 0">
+                  <Text fontSize="sm" fontWeight="bold">
+                    {artist.name}
+                  </Text>
+                  <Text fontSize="x-small" color="gray.400">
+                    Artist
+                  </Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
-}
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+  return {
+    props: { artists },
+  };
+};
+export default Home;
